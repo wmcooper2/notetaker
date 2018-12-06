f7f52b112b1597b24705d29aba1b1566b7b472b4
wmc2tokyo <wmcooper2@yahoo.com>
2018-12-06 20:54:35 +0900

    First commit.

---
+++ src/notetaker.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Takes quick notes from the command line."""
+
+#stand lib
+import argparse as ap
+
+#cutom
+from constants import HOME
+
+save_file = HOME+"Desktop/QuickNotes.txt"
+
+if __name__ == "__main__":
+    parser = ap.ArgumentParser(description="Take quick notes from command line.")
+    parser.add_argument("note", help="Saves a note.")
+
+    args = parser.parse_args()
+
+    if args:
+        with open(save_file, "a+") as f:
+            f.write(args.note)
+            f.write("\n")
+    
+    parser.exit(status=0, message="Note saved.\n")
+#    print("\n")
